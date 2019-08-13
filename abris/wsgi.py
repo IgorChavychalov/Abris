b8895d601cc04ffbf0f9d0a5f3d9eb655be4a01c
Protor_fenix <Protor_fenix@mail.ru>
2019-08-14 00:49:09 +0300

    сервер: -создан джанго -подключил index.html + css + js

---
+++ abris/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for abris project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'abris.settings')
+
+application = get_wsgi_application()
