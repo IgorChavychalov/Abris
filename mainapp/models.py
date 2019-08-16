3e97ce1e7ed8655030db2a054c19397e1378a48f
Protor_fenix <Protor_fenix@mail.ru>
2019-08-17 00:12:36 +0300

    сервер: -созданна модель с текстовым полем

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.core.validators import validate_comma_separated_integer_list
+# from django.contrib.gis.db import models
 
-# Create your models here.
+
+class Draw(models.Model):
+    name = models.CharField(verbose_name='название', max_length=64, unique=True)
+    forestry = models.CharField(verbose_name='лесничество', max_length=32, unique=True)
+    quarter = models.CharField(verbose_name='квартал', max_length=4)
+    letter = models.CharField(verbose_name='выдел', max_length=4)
+    # rast = models.RasterField()
+    rast = models.ImageField(upload_to='rast', blank=True)
+
+    def __str__(self):
+        return f"{self.name} {self.forestry} {self.quarter}-{self.letter}"
+
+
+class Polygons(models.Model):
+    draw = models.ForeignKey(Draw, on_delete=models.CASCADE)
+    name = models.CharField(verbose_name='название', max_length=64, unique=True)
+    # coordinates = models.LineStringField()
+    coordinates = models.TextField(verbose_name='координаты', null=True)
+    # coordinates = models.CommaSeparatedIntegerField(verbose_name='координаты', max_length=600)
+    operating = models.BooleanField(verbose_name='основной', default=True)
+
+    def __str__(self):
+        return f"{self.name} {self.operating}"
